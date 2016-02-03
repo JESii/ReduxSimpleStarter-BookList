67c90622d863a45feb2e6e563d282eea233f560d
Jon Seidel <jseidel@edpci.com>
2016-02-02 18:01:32 -0800

    Add simple book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript: the good parts'},
-    {title: 'Understanding why AngularJS sucks'},
-    {title: 'The Mythical Man-Month'},
-    {title: 'The Money Trap'}
+    {title: 'Javascript: the good parts', pages: 101 },
+    {title: 'Understanding why AngularJS sucks', pages: 401 },
+    {title: 'The Mythical Man-Month', pages: 347 },
+    {title: 'The Money Trap', pages: 17 }
   ]
 }
